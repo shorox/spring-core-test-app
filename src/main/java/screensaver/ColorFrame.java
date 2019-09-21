9fc8d24f1d3d3122c47f16b93564abda6b489ec7
shorox <skazo4nix@gmail.com>
2019-09-21 16:33:51 +0300

    add screensaver

---
+++ src/main/java/screensaver/ColorFrame.java
@@ -0,0 +1,26 @@
+package screensaver;
+
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+@Component
+public abstract class ColorFrame extends JFrame {
+
+  public ColorFrame() {
+    setSize(200, 200);
+    setVisible(true);
+    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+  }
+
+  public void showOnRandomPlace() {
+    Random random = new Random();
+    setLocation(random.nextInt(1200), random.nextInt(600));
+    getContentPane().setBackground(getColor());
+    repaint();
+  }
+
+  protected abstract Color getColor();
+}
